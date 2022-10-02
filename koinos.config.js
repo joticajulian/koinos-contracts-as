33335572a6300b9105848ca408ce85f4f40186aa
Julian Gonzalez <joticajulian@gmail.com>
2022-10-02 06:09:43 +0000

    update precompiler, move token folder to contracts

--- koinos.config.js
+++ koinos.config.js
@@ -2,4 +2,6 @@ module.exports = {
   class: "Token",
   proto: ["./proto/token.proto"],
   files: ["./Token.ts"],
+  sourceDir: "./contracts/token/assembly",
+  // koinosProtoDir: "../../node_modules/koinos-precompiler-as/koinos-proto",
 };

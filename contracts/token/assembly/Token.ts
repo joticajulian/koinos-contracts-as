48cf09200f97069c0c96477b9144659b2d06375d
Julian Gonzalez <joticajulian@gmail.com>
2023-04-18 19:20:42 +0000

    wip - updates token contract

--- contracts/token/assembly/Token.ts
+++ contracts/token/assembly/Token.ts
@@ -1,8 +1,22 @@
-import { Arrays, System, authority, Storage } from "@koinos/sdk-as";
+import {
+  Arrays,
+  System,
+  authority,
+  Storage,
+  Protobuf,
+  Base58,
+  StringBytes,
+  value,
+  Crypto,
+} from "@koinos/sdk-as";
 import { token } from "./proto/token";
 
-const SUPPLY_ID = 0;
+const SUPPLY_SPACE_ID = 0;
 const BALANCES_SPACE_ID = 1;
+const ALLOWANCES_SPACE_ID = 2;
+const USER_CONTRACTS_SPACE_ID = 3;
+
+export const AUTHORIZE_CONTRACT_CALL_ENTRY_POINT = 0x10e5820f; // authorize_contract_call
 
 export class Token {
   callArgs: System.getArgumentsReturn | null;
@@ -14,12 +28,14 @@ export class Token {
   contractId: Uint8Array;
   supply: Storage.Obj<token.uint64>;
   balances: Storage.Map<Uint8Array, token.uint64>;
+  allowances: Storage.Map<Uint8Array, token.uint64>;
+  userContracts: Storage.Map<Uint8Array, token.boole>;
 
   constructor() {
     this.contractId = System.getContractId();
     this.supply = new Storage.Obj(
       this.contractId,
-      SUPPLY_ID,
+      SUPPLY_SPACE_ID,
       token.uint64.decode,
       token.uint64.encode,
       () => new token.uint64(0)
@@ -30,6 +46,20 @@ export class Token {
       token.uint64.decode,
       token.uint64.encode,
       () => new token.uint64(0)
+    );
+    this.allowances = new Storage.Map(
+      this.contractId,
+      ALLOWANCES_SPACE_ID,
+      token.uint64.decode,
+      token.uint64.encode,
+      null
+    );
+    this.userContracts = new Storage.Map(
+      this.contractId,
+      USER_CONTRACTS_SPACE_ID,
+      token.boole.decode,
+      token.boole.encode,
+      () => new token.boole(false)
     );
   }
 
@@ -87,121 +117,262 @@ export class Token {
     return this.balances.get(args.owner!)!;
   }
 
+  // TODO: add this function to the SDK
+  getSigners(): Array<Uint8Array> {
+    const sigBytes =
+      System.getTransactionField("signatures")!.message_value!.value!;
+    const signatures = Protobuf.decode<value.list_type>(
+      sigBytes,
+      value.list_type.decode
+    );
+    const txId = System.getTransactionField("id")!.bytes_value!;
+
+    const signers: Array<Uint8Array> = [];
+    for (let i = 0; i < signatures.values.length; i++) {
+      const publicKey = System.recoverPublicKey(
+        signatures.values[i].bytes_value!,
+        txId
+      );
+      const address = Crypto.addressFromPublicKey(publicKey!);
+      signers.push(address);
+    }
+    return signers;
+  }
+
+  /**
+   * Internal function to call the contract of an account to request its
+   * authority to perform an operation.
+   */
+  is_authorized_by_contract_account(account: Uint8Array): boolean {
+    const caller = System.getCaller();
+    const callRes = System.call(
+      account,
+      AUTHORIZE_CONTRACT_CALL_ENTRY_POINT,
+      Protobuf.encode(
+        new authority.authorize_arguments(
+          authority.authorization_type.contract_call,
+          new authority.call_data(
+            this.contractId,
+            this.callArgs!.entry_point,
+            caller.caller,
+            this.callArgs!.args
+          )
+        ),
+        authority.authorize_arguments.encode
+      )
+    );
+    if (callRes.code != 0) {
+      const errorMessage = `failed to call contract of ${Base58.encode(
+        account
+      )}: ${
+        callRes.res.error && callRes.res.error!.message
+          ? callRes.res.error!.message!
+          : "unknown error"
+      }`;
+      System.exit(callRes.code, StringBytes.stringToBytes(errorMessage));
+    }
+    System.require(
+      callRes.res.object,
+      `empty response from ${Base58.encode(account)}`
+    );
+
+    return Protobuf.decode<authority.authorize_result>(
+      callRes.res.object!,
+      authority.authorize_result.decode
+    ).value;
+  }
+
+  /**
+   * Internal function to validate the authority of an operation.
+   * This function replaces the koinos native function called
+   * "System.requireAuthority()". And it introduces new features to
+   * increase the security of the contract.
+   *
+   * Why is this needed? let's take a look to the logic of
+   * System.requireAuthority():
+   *
+   * - If the user has a smart contract wallet (and if it was
+   *   tagged to resolve contract calls) then that contract is called.
+   * - Otherwise the system will check if the transaction was signed
+   *   by the user. This second point is risky because the flow of contract
+   *   calls could be: A -> B -> C -> D. The user approved the operation
+   *   in "A", but he doesn't know what will happen in B, C, or D and his
+   *   signature is still in the transaction. Then some malicious contract
+   *   in the middle could take advantage of this point to steal the assets.
+   *
+   * What changed in this check_authority function? It implements approvals
+   * and checks who is the caller:
+   *
+   * - If there is a caller (that is, if this operation was not triggered
+   *   by the user itself but by some contract in the middle), it is approved
+   *   if one of the following conditions are met:
+   *     1. The caller is approved by the user (for all tokens or single
+   *        token).
+   *     2. The caller is the user.
+   *     3. The user has a contract and this contract authorizes the operation.
+   * - If there is NO caller (that is, if this operation appears in the list
+   *   of operations in the transaction, not called by some contract in the
+   *   middle) then the contract will check if one of the following conditions
+   *   are met:
+   *     1. The transaction is signed by an account approved by the user (for
+   *        all tokens or single token).
+   *     2. The user has a contract and this contract authorizes the operation.
+   *     3. The transaction is signed by the user, but with the condition that
+   *        the user doesn't have a contract.
+   *
+   * Note 1: The approvals are granted by the user by using "approve" and
+   * "set_approva_for_all" functions.
+   *
+   * Note 2: Currently there is no a system call to check if an account has
+   * a smart contract wallet or not. Then as a temporal solution, the user has
+   * to call "set_authority_contract" to define that he uses a smart contract
+   * wallet.
+   */
+  check_authority(
+    account: Uint8Array,
+    acceptAllowances: boolean,
+    amount: u64
+  ): boolean {
+    const caller = System.getCaller();
+
+    const key = new Uint8Array(50);
+    if (acceptAllowances) {
+      key.set(account, 0);
+    }
+
+    // check if there is a caller (smart contract in the middle)
+    if (caller.caller && caller.caller!.length > 0) {
+      if (acceptAllowances) {
+        // check if the caller is approved for all tokens
+        key.set(caller.caller!, 25);
+        const allowance = this.allowances.get(key);
+        if (allowance && allowance.value >= amount) return true;
+      }
+
+      // check if the account is the caller
+      if (Arrays.equal(account, caller.caller)) return true;
+
+      // check if the contract of the account authorizes the operation
+      // TODO: to be replaced by the system call to get contract metadata
+      if (this.userContracts.get(account)!.value == true) {
+        System.log("Account contract called to resolve the authority");
+        return this.is_authorized_by_contract_account(account);
+      }
+
+      // the transaction has a caller but none of the different
+      // options authorized the operation, then it is rejected.
+      return false;
+    }
+
+    // check the signatures related to allowances
+    const signers = this.getSigners();
+    for (let i = 0; i < signers.length; i += 1) {
+      if (acceptAllowances) {
+        // check if the signer is approved for all tokens
+        key.set(signers[i], 25);
+        const allowance = this.allowances.get(key);
+        if (allowance && allowance.value >= amount) return true;
+      }
+    }
+
+    // check if the account has a contract
+    if (this.userContracts.get(account)!.value == true) {
+      // consult the contract of the account
+      System.log("Account contract called to resolve the authority");
+      return this.is_authorized_by_contract_account(account);
+    }
+
+    // there is no caller, no approval from allowances, and the account
+    // doesn't have a contract then check if the account signed the transaction
+    for (let i = 0; i < signers.length; i += 1) {
+      if (Arrays.equal(account, signers[i])) return true;
+    }
+
+    // none of the different options authorized the operation,
+    // then it is rejected.
+    return false;
+  }
+
+  _transfer(args: token.transfer_args): void {
+    let fromBalance = this.balances.get(args.from!)!;
+    System.require(
+      fromBalance.value >= args.value,
+      "account 'from' has insufficient balance"
+    );
+    fromBalance.value -= args.value;
+    this.balances.put(args.from!, fromBalance);
+
+    let toBalance = this.balances.get(args.to!)!;
+    toBalance.value += args.value;
+    this.balances.put(args.to!, toBalance);
+
+    const impacted = [args.to!, args.from!];
+    System.event(
+      "koinos.contracts.token.transfer_event",
+      Protobuf.encode<token.transfer_args>(args, token.transfer_args.encode),
+      impacted
+    );
+  }
+
+  _mint(args: token.mint_args): void {
+    const supply = this.supply.get()!;
+    System.require(
+      supply.value <= u64.MAX_VALUE - args.value,
+      "mint would overflow supply"
+    );
+
+    let toBalance = this.balances.get(args.to!)!;
+    toBalance.value += args.value;
+    this.balances.put(args.to!, toBalance);
+    supply.value += args.value;
+    this.supply.put(supply);
+
+    const impacted = [args.to!];
+    System.event(
+      "koinos.contracts.token.mint_event",
+      Protobuf.encode<token.mint_args>(args, token.mint_args.encode),
+      impacted
+    );
+  }
+
+  _burn(args: token.burn_args): void {
+    let fromBalance = this.balances.get(args.from!)!;
+    System.require(
+      fromBalance.value >= args.value,
+      "account 'from' has insufficient balance"
+    );
+
+    const supply = this.supply.get()!;
+    fromBalance.value -= args.value;
+    this.balances.put(args.from!, fromBalance);
+    supply.value -= args.value;
+    this.supply.put(supply);
+
+    const impacted = [args.from!];
+    System.event(
+      "koinos.contracts.token.burn_event",
+      Protobuf.encode<token.burn_args>(args, token.burn_args.encode),
+      impacted
+    );
+  }
+
   /**
    * Transfer tokens
    * @external
    */
-  transfer(args: token.transfer_args): token.boole {
-    const from = args.from!;
-    const to = args.to!;
-    const value = args.value;
-
-    const caller = System.getCaller();
-    if (
-      !Arrays.equal(from, caller.caller) &&
-      !System.checkAuthority(
-        authority.authorization_type.contract_call,
-        from,
-        this.callArgs!.args
-      )
-    ) {
-      System.log("from has not authorized transfer");
-      return new token.boole(false);
-    }
-
-    let fromBalance = this.balances.get(from)!;
-    if (fromBalance.value < value) {
-      System.log("'from' has insufficient balance");
-      return new token.boole(false);
-    }
-    fromBalance.value -= value;
-    this.balances.put(from, fromBalance);
-
-    let toBalance = this.balances.get(to)!;
-    toBalance.value += value;
-    this.balances.put(to, toBalance);
-
-    const impacted = [to, from];
-    System.event("token.transfer", this.callArgs!.args, impacted);
-
-    return new token.boole(true);
+  transfer(args: token.transfer_args): void {
+    const isAuthorized = this.check_authority(args.from!, true, args.value);
+    System.require(isAuthorized, "from has not authorized transfer");
+    this._transfer(args);
   }
 
   /**
    * Mint new tokens
    * @external
    */
-  mint(args: token.mint_args): token.boole {
-    const to = args.to!;
-    const value = args.value;
-
-    const caller = System.getCaller();
-    if (
-      !Arrays.equal(this.contractId, caller.caller) &&
-      !System.checkAuthority(
-        authority.authorization_type.contract_call,
-        this.contractId,
-        this.callArgs!.args
-      )
-    ) {
-      System.log("contract has not authorized mint");
-      return new token.boole(false);
-    }
-
-    const supply = this.supply.get()!;
-    if (supply.value > u64.MAX_VALUE - value) {
-      System.log("Mint would overflow supply");
-      return new token.boole(false);
-    }
-
-    let toBalance = this.balances.get(to)!;
-    toBalance.value += value;
-    this.balances.put(to, toBalance);
-    supply.value += value;
-    this.supply.put(supply);
-
-    const impacted = [to];
-    System.event("token.mint", this.callArgs!.args, impacted);
-
-    return new token.boole(true);
-  }
-
-  /**
-   * Burn tokens
-   * @external
-   */
-  burn(args: token.burn_args): token.boole {
-    const from = args.from!;
-    const value = args.value;
-
-    const caller = System.getCaller();
-    if (
-      !Arrays.equal(from, caller.caller) &&
-      !System.checkAuthority(
-        authority.authorization_type.contract_call,
-        from,
-        this.callArgs!.args
-      )
-    ) {
-      System.log("from has not authorized burn");
-      return new token.boole(false);
-    }
-
-    let fromBalance = this.balances.get(from)!;
-    if (fromBalance.value < value) {
-      System.log("'from' has insufficient balance");
-      return new token.boole(false);
-    }
-
-    const supply = this.supply.get()!;
-    fromBalance.value -= value;
-    this.balances.put(from, fromBalance);
-    supply.value -= value;
-    this.supply.put(supply);
-
-    const impacted = [from];
-    System.event("token.burn", this.callArgs!.args, impacted);
-
-    return new token.boole(true);
+  mint(args: token.mint_args): void {
+    const isAuthorized = this.check_authority(this.contractId, false, 0);
+    System.require(isAuthorized, "owner has not authorized mint");
+    this._mint(args);
   }
 }
--- contracts/token/scripts/mint.ts
+++ contracts/token/scripts/mint.ts
@@ -1,44 +1,45 @@
 import { Signer, Contract, Provider } from "koilib";
 import { TransactionJson } from "koilib/lib/interface";
-import * as dotenv from "dotenv";
-import tokenAbi from "../build/token-abi.json";
+import abi from "../build/token-abi.json";
+import koinosConfig from "../koinos.config.js";
 
-dotenv.config();
-
-const privateKeyManaSupporter = process.env.PRIVATE_KEY_MANA_SUPPORTER ?? "";
-const privateKeyContract = process.env.PRIVATE_KEY_CONTRACT ?? "";
+const [inputNetworkName] = process.argv.slice(2);
 
 async function main() {
-  const provider = new Provider(["http://api.koinos.io:8080"]);
-  // const provider = new Provider(["https://api.koinosblocks.com"]);
-  const accountWithFunds = Signer.fromWif(privateKeyManaSupporter);
-  const contractAccount = Signer.fromWif(privateKeyContract);
+  const networkName = inputNetworkName || "harbinger";
+  const network = koinosConfig.networks[networkName];
+  if (!network) throw new Error(`network ${networkName} not found`);
+  const provider = new Provider(network.rpcNodes);
+  const accountWithFunds = Signer.fromWif(
+    network.accounts.manaSharer.privateKey
+  );
+  const contractAccount = Signer.fromWif(network.accounts.contract.privateKey);
   accountWithFunds.provider = provider;
   contractAccount.provider = provider;
 
   const contract = new Contract({
-    id: contractAccount.address,
     signer: contractAccount,
     provider,
-    abi: tokenAbi,
+    abi,
     options: {
       payer: accountWithFunds.address,
+      rcLimit: "10000000000",
       beforeSend: async (tx: TransactionJson) => {
         await accountWithFunds.signTransaction(tx);
       },
     },
   });
 
-  const params = {
-    to: contractAccount.address,
-    value: "123456789",
-  };
-  const { receipt, transaction } = await contract.functions.mint(params);
+  const { receipt, transaction } = await contract.functions.mint({
+    to: accountWithFunds.address,
+    value: "100",
+  });
   console.log("Transaction submitted. Receipt: ");
   console.log(receipt);
   const { blockNumber } = await transaction.wait("byBlock", 60000);
-  console.log(`New coins minted (block number ${blockNumber})`);
-  console.log(params);
+  console.log(
+    `Transaction mined in block number ${blockNumber} (${networkName})`
+  );
 }
 
 main()

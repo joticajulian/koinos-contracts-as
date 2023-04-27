b85a255ba4eb7c38a1cb85d23cbdd628e94bb6b8
motoeng <kuixihe@gmail.com>
2023-04-27 12:36:29 -0400

    motoengineer updates

--- contracts/hello/koinos.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  class: "Hello",
-  proto: ["./proto/hello.proto"],
-  files: ["./Hello.ts"],
-  sourceDir: "./assembly",
-  buildDir: "./build",
-  koinosProtoDir: "../../node_modules/koinos-precompiler-as/koinos-proto",
-};

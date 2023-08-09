113da30233cdb8ab6ed9923e0cc87b9b0031cdfe
Julian Gonzalez <joticajulian@gmail.com>
2023-08-09 17:09:14 +0000

    chore: prepare package

--- updateDist.js
+++ updateDist.js
@@ -2,16 +2,16 @@ const fs = require("fs");
 const fse = require("fs-extra");
 const path = require("path");
 
-if (!fs.existsSync("./dist")) {
-  fs.mkdirSync("dist");
+if (!fs.existsSync("./assembly")) {
+  fs.mkdirSync("assembly");
 }
 
+let index = "";
 const contracts = fs.readdirSync("./contracts", { withFileTypes: true });
 contracts.forEach((contract) => {
   if (!contract.isDirectory()) return;
-  if (contract.name !== "token") return;
   const src = path.join("./contracts", contract.name, "./build");
-  const dest = path.join("./dist", contract.name);
+  const dest = path.join("./assembly", contract.name);
   fs.rmdirSync(dest, { recursive: true, force: true });
   fse.copySync(src, dest);
   if (fs.existsSync(path.join(dest, "proto"))) {
@@ -24,4 +24,34 @@ contracts.forEach((contract) => {
       force: true,
     });
   }
+  // update index
+  const contractFiles = fs.readdirSync(src);
+  contractFiles.forEach(contractFile => {
+    if (contractFile.endsWith(".ts") && contractFile !== "index.ts" && contractFile !== "constants.ts") {
+      const className = contractFile.replace(".ts", "");
+      index += `\nexport { ${className} } from "./${contract.name}/${className}";`;
+    }
+
+    if (contractFile === "proto") {
+      const protoFiles = fs.readdirSync(path.join(src, "proto"));
+      protoFiles.forEach(protoFile => {
+        if (protoFile.endsWith(".ts")) {
+          const className = protoFile.replace(".ts", "");
+          index += `\nexport { ${className} } from "./${contract.name}/proto/${className}";`;
+        }
+      });
+    }
+
+    if (contractFile === "interfaces") {
+      const interfaces = fs.readdirSync(path.join(src, "interfaces"));
+      interfaces.forEach(interface => {
+        if (interface.endsWith(".ts")) {
+          const className = interface.replace(".ts", "");
+          index += `\nexport { ${className.slice(1)} as ${className} } from "./${contract.name}/interfaces/${className}";`;
+        }
+      });
+    }
+  });
+
+  fs.writeFileSync("./assembly/index.ts", index);
 });

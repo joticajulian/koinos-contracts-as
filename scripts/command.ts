b4cfa23b097e4c681630d3a6b79da7820aca30f2
Julian Gonzalez <joticajulian@gmail.com>
2023-09-22 15:47:14 +0000

    option to build all

--- scripts/command.ts
+++ scripts/command.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import { asbuild, precompile } from "./compiler";
 
 const [command] = process.argv.slice(2);
 
@@ -7,13 +8,26 @@ async function main() {
   switch (command) {
     case "clear-builds": {
       const contractsPath = path.join(__dirname, "../contracts");
-      const contracts = fs.readdirSync(contractsPath, { withFileTypes: true });
+      const contracts = fs
+        .readdirSync(contractsPath, { withFileTypes: true })
+        .filter((c) => c.isDirectory());
       contracts.forEach((contract) => {
-        if (!contract.isDirectory()) return;
         fs.rmdirSync(path.join(contractsPath, contract.name, "build"), {
           recursive: true,
         });
       });
+      break;
+    }
+    case "build-all": {
+      const contractsPath = path.join(__dirname, "../contracts");
+      const contracts = fs
+        .readdirSync(contractsPath, { withFileTypes: true })
+        .filter((c) => c.isDirectory());
+      for (let i = 0; i < contracts.length; i += 1) {
+        const contractName = contracts[0].name;
+        await precompile(contractName, contractName);
+        await asbuild(contractName, contractName);
+      }
       break;
     }
     default: {

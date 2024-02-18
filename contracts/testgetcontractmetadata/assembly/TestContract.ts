4d38595aa8234175aed28582e41e010d37c68315
Julian Gonzalez <joticajulian@gmail.com>
2024-02-18 01:45:41 +0100

    bump koinosbox/sdk-as lib

--- contracts/testgetcontractmetadata/assembly/TestContract.ts
+++ contracts/testgetcontractmetadata/assembly/TestContract.ts
@@ -14,7 +14,7 @@ export class TestContract {
    * @external
    */
   operate_assets(args: common.address): common.boole {
-    const isAuthorized = System.checkCallContractAuthority(args.value!);
+    const isAuthorized = System.checkAuthority(args.value!);
     System.require(isAuthorized, "not authorized");
     System.log("authorized to operate with the assets");
     return new common.boole(true);

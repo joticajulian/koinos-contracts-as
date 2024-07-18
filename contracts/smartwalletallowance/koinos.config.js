9018d9877490952e511c6c4d712a6c7c39ee93da
Julian Gonzalez <joticajulian@gmail.com>
2024-07-18 18:14:31 +0200

    smart wallet allowance

---
+++ contracts/smartwalletallowance/koinos.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+
+module.exports = {
+  class: "SmartWalletAllowance",
+  version: "1.0.0",
+  supportAbi1: true,
+  proto: ["./proto/smartwalletallowance.proto"],
+  files: ["./SmartWalletAllowance.ts"],
+  sourceDir: "./assembly",
+  buildDir: "./build",
+  protoImport: [
+    {
+      name: "@koinosbox/contracts",
+      path:
+        process.env.IMPORT_KOINOSBOX_PROTO_FROM_NODE_MODULES === "true"
+          ? "../../node_modules/@koinosbox/contracts/koinosbox-proto"
+          : "../../koinosbox-proto",
+      exclude: ["smartwalletallowance"],
+    },
+    {
+      name: "@koinos/sdk-as",
+      path: "../../node_modules/koinos-precompiler-as/koinos-proto/koinos",
+    },
+    {
+      name: "__",
+      path: "../../node_modules/koinos-precompiler-as/koinos-proto/google",
+    },
+  ],
+  deployOptions: {
+    authorizesTransactionApplication: true,
+  },
+  networks: {
+    harbinger: {
+      rpcNodes: [
+        "https://harbinger-api.koinos.io",
+        "https://testnet.koinosblocks.com",
+      ],
+      accounts: {
+        manaSharer: {
+          privateKey: process.env.HARBINGER_MANA_SHARER_PRIVATE_KEY,
+          managers: process.env.HARBINGER_MANAGERS,
+        },
+      },
+    },
+    mainnet: {
+      rpcNodes: ["https://api.koinosblocks.com", "https://api.koinos.io"],
+      accounts: {
+        manaSharer: {
+          privateKey: process.env.MAINNET_MANA_SHARER_PRIVATE_KEY,
+          managers: process.env.MAINNET_MANAGERS,
+        },
+      },
+    },
+  },
+};

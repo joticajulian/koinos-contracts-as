691a3ba27d8a8d9e385872b9d6e7d970195e7d53
Julian Gonzalez <joticajulian@gmail.com>
2024-08-09 08:55:13 +0200

    update assembly packages, update ethaddress

--- contracts/ethaddress/assembly/Ethaddress.ts
+++ contracts/ethaddress/assembly/Ethaddress.ts
@@ -23,7 +23,10 @@ export class Ethaddress extends Nft {
   _symbol: string = "ETHADDRESS";
   _uri: string = "";
 
-  checkMessageSignedByEthAddress(message: string, ethAddress: Uint8Array): boolean {
+  checkMessageSignedByEthAddress(
+    message: string,
+    ethAddress: Uint8Array
+  ): boolean {
     const sigBytes =
       System.getTransactionField("signatures")!.message_value!.value!;
     const signatures = Protobuf.decode<value.list_type>(
@@ -31,11 +34,23 @@ export class Ethaddress extends Nft {
       value.list_type.decode
     );
     const ethMessage = `\x19Ethereum Signed Message:\n${message.length}${message}`;
-    let multihashBytes = System.hash(Crypto.multicodec.keccak_256, StringBytes.stringToBytes(ethMessage));
+    let multihashBytes = System.hash(
+      Crypto.multicodec.keccak_256,
+      StringBytes.stringToBytes(ethMessage)
+    );
 
     for (let i = 0; i < signatures.values.length; i++) {
-      const publicKey = System.recoverPublicKey(signatures.values[i].bytes_value, multihashBytes!, chain.dsa.ecdsa_secp256k1, false);
-      multihashBytes = System.hash(Crypto.multicodec.keccak_256, publicKey!.subarray(1));
+      if (signatures.values[i].bytes_value.length != 65) continue;
+      const publicKey = System.recoverPublicKey(
+        signatures.values[i].bytes_value,
+        multihashBytes!,
+        chain.dsa.ecdsa_secp256k1,
+        false
+      );
+      multihashBytes = System.hash(
+        Crypto.multicodec.keccak_256,
+        publicKey!.subarray(1)
+      );
       let mh = new Crypto.Multihash();
       mh.deserialize(multihashBytes!);
       if (Arrays.equal(mh.digest.subarray(-20), ethAddress)) return true;
@@ -48,10 +63,13 @@ export class Ethaddress extends Nft {
    * @external
    */
   mint(args: nft.mint_args): void {
-    const message = "";
-    const authorized = this.checkMessageSignedByEthAddress("TODO", args.token_id);
+    const message = `link eth address ${4}`;
+    const authorized = this.checkMessageSignedByEthAddress(
+      "TODO",
+      args.token_id
+    );
     if (!authorized) {
-      System.fail("not signed by the ethereum address")
+      System.fail("not signed by the ethereum address");
     }
   }
 }
--- contracts/ethaddress/assembly/__tests__/ethaddress.spec.ts
+++ contracts/ethaddress/assembly/__tests__/ethaddress.spec.ts
@@ -1,9 +1,4 @@
-import {
-  Base58,
-  MockVM,
-  chain,
-  system_calls,
-} from "@koinos/sdk-as";
+import { Base58, MockVM, chain, system_calls } from "@koinos/sdk-as";
 
 const CONTRACT_ID = Base58.decode("1DQzuCcTKacbs9GGScRTU1Hc8BsyARTPqe");
 const USER_ID = Base58.decode("1FkSxrCK6D3ELi2taw8QLgxPMBUnnoxfgy");
@@ -17,5 +12,15 @@ describe("Ethaddress", () => {
       new chain.result(new Uint8Array(0))
     );
     MockVM.setCallContractResults([result, result, result]);
+  });
+
+  it("test hex string", () => {
+    const address = new Uint8Array(7);
+    address.set([0xfe, 0x35, 0xc4, 0x4a, 0x00, 0x04, 0x50]);
+    let hex = "";
+    for (let i = 0; i < address.length; i += 1) {
+      hex += address[i].toString(16);
+    }
+    expect(hex).toBe("4");
   });
 });
